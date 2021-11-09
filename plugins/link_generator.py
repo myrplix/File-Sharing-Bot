cc91dd410979a4f6f4b6bc75a925c6cd086538ee
myrplix <93067102+myrplix@users.noreply.github.com>
2021-11-09 17:27:02 +0800

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,6 +37,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b></b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
